c88d73b509d97bcc0748d5db655c0310286f92bd
developspeed <gerard@dataspeed.nl>
2023-03-10 18:43:24 +0530

    new functionality

--- SpeechToText.py
+++ SpeechToText.py
@@ -5,11 +5,7 @@ from audio_recorder_streamlit import audio_recorder
 from io import BytesIO
 from deta import Deta   
 
-st.set_page_config(
-    page_title="Speech Recognizer",
-    # page_icon=im,
-    layout="wide",
-)
+
 
 hide_menu = """
 <style>
@@ -40,7 +36,7 @@ def getItem():
 ######################### Frontend UI of the Application #########################
 
 # App Title Name
-st.title("Speech Recognizer")
+st.title("Speech to Text")
 
 
 key = st.sidebar.text_input("Enter the Security Key")
@@ -69,8 +65,6 @@ if key == auth[4]["secretKey"]:
 
     # Uploading audio file
 
-    # audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
-
     # Taking audio file from mic
     audio_file = audio_recorder(
         text="Click the mic to record your audio -  ",
@@ -81,9 +75,18 @@ if key == auth[4]["secretKey"]:
         pause_threshold=4.0
     )
 
+    st.write("__________________________________________________________________")
+
+    # uploading files
+    audio_upload = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+
+
     # Playing audio file
     if audio_file:
         st.audio(audio_file, format="audio/wav")
+    
+    if audio_upload:
+        st.audio(audio_upload, format="audio/wav")
 
     # converting bytes to ioBytes object
     newAudio = BytesIO(audio_file)
@@ -106,13 +109,93 @@ if key == auth[4]["secretKey"]:
     st.sidebar.write('Language spoken in the audio, specify None to perform language detection')
     option = st.sidebar.selectbox('Choose Language',("","af","am","ar","as","az","ba","be","bg","bn","bo","br","bs","ca","cs","cy","da","de","el","en","es","et","eu","fa","fi","fo","fr","gl","gu","ha","haw","hi","hr","ht","hu","hy","id","is","it","iw","ja","jw","ka","kk","km","kn","ko","la","lb","ln","lo","lt","lv","mg","mi","mk","ml","mn","mr","ms","mt","my","ne","nl","nn","no","oc","pa","pl","ps","pt","ro","ru","sa","sd","si","sk","sl","sn","so","sq","sr","su","sv","sw","ta","te","tg","th","tk","tl","tr","tt","uk","ur","uz","vi","yi","yo","zh","Afrikaans","Albanian","Amharic","Arabic","Armenian","Assamese","Azerbaijani","Bashkir","Basque","Belarusian","Bengali","Bosnian","Breton","Bulgarian","Burmese","Castilian","Catalan","Chinese","Croatian","Czech","Danish","Dutch","English","Estonian","Faroese","Finnish","Flemish","French","Galician","Georgian","German","Greek","Gujarati","Haitian","Creole","Hausa","Hawaiian","Hebrew","Hindi","Hungarian","Icelandic","Indonesian","Italian","Japanese","Javanese","Kannada","Kazakh","Khmer","Korean","Lao","Latin","Latvian","Letzeburgesch","Lingala","Lithuanian","Luxembourgish","Macedonian","Malagasy","Malay","Malayalam","Maltese","Maori","Marathi","Moldavian","Moldovan","Mongolian","Myanmar","Nepali","Norwegian","Nynorsk","Occitan","Panjabi","Pashto","Persian","Polish","Portuguese","Punjabi","Pushto","Romanian","Russian","Sanskrit","Serbian","Shona","Sindhi","Sinhala","Sinhalese","Slovak","Slovenian","Somali","Spanish","Sundanese","Swahili","Swedish","Tagalog","Tajik","Tamil","Tatar","Telugu","Thai","Tibetan","Turkish","Turkmen","Ukrainian","Urdu","Uzbek","Valencian","Vietnamese","Welsh","Yiddish","Yoruba"))
 
-
+    st.write("__________________________________________________________________")
     if st.button("Transcribe Audio"):
         if audio_file is not None:
             databaseValue = getItem()
             inputs = {
                 # Audio file
                 'audio': newAudio,
+
+                # Choose a Whisper model.
+                'model': databaseValue[1]["model"],
+
+                # Choose the format for the transcription
+                'transcription': databaseValue[1]["transcription"],
+
+                # Translate the text to English when set to True
+                'translate': True,
+
+                # language spoken in the audio, specify None to perform language
+                # detection
+                # 'language': ...,
+
+                # temperature to use for sampling
+                'temperature': databaseValue[1]["temperature"],
+
+                # optional patience value to use in beam decoding, as in
+                # https://arxiv.org/abs/2204.05424, the default (1.0) is equivalent to
+                # conventional beam search
+                'patience': databaseValue[1]["patience"],
+
+                # comma-separated list of token ids to suppress during sampling; '-1'
+                # will suppress most special characters except common punctuations
+                'suppress_tokens': databaseValue[1]["suppress_tokens"],
+
+                # optional text to provide as a prompt for the first window.
+                'initial_prompt': databaseValue[1]["initial_prompt"],
+
+                # if True, provide the previous output of the model as a prompt for
+                # the next window; disabling may make the text inconsistent across
+                # windows, but the model becomes less prone to getting stuck in a
+                # failure loop
+                'condition_on_previous_text': databaseValue[1]["checkBoxVal"],
+
+                # temperature to increase when falling back when the decoding fails to
+                # meet either of the thresholds below
+                'temperature_increment_on_fallback': databaseValue[1]["temperature_increment_on_fallback"],
+
+                # if the gzip compression ratio is higher than this value, treat the
+                # decoding as failed
+                'compression_ratio_threshold': databaseValue[1]["compression_ratio_threshold"],
+
+                # if the average log probability is lower than this value, treat the
+                # decoding as failed
+                'logprob_threshold': databaseValue[1]["logprob_threshold"],
+
+                # if the probability of the <|nospeech|> token is higher than this
+                # value AND the decoding has failed due to `logprob_threshold`,
+                # consider the segment as silence
+                'no_speech_threshold': databaseValue[1]["no_speech_threshold"],
+            }
+
+            # https://replicate.com/openai/whisper/versions/30414ee7c4fffc37e260fcab7842b5be470b9b840f2b608f5baa9bbef9a259ed#output-schema
+            try:
+                st.warning("Transcribing Audio...")
+                output = version.predict(**inputs)
+                # print(output)
+                # Performing the Analysis
+                output = version.predict(**inputs)
+
+                # Audio Language Detection
+                st.write("Detected Language : ", output['detected_language'])
+                
+                st.subheader("Output")
+                st.text_area(" ",value=output["transcription"])
+
+                # Translated Item
+                st.checkbox("Translate",value=True)
+                st.text_area("English Translated",value=output["translation"])
+                st.success("Transcription Completed!")
+            
+            except Exception:
+                st.error("Looks like some parameters are incorrect in settings or API expired.")
+        
+        if audio_upload is not None:
+            databaseValue = getItem()
+            inputs = {
+                # Audio file
+                'audio': audio_upload,
 
                 # Choose a Whisper model.
                 'model': databaseValue[1]["model"],
@@ -194,26 +277,3 @@ elif key == "":
     st.sidebar.warning("Enter Secret Key")
 else:
     st.sidebar.error("Incorrect Secret Key")
-
-    
-   
-# Magic AI dashboard
-st.write("__________________________________________________________________________________________")
-link = '[Magicaibox](https://www.magicaibox.site/controlpanel/udashboard)'
-html = """
-    <style>
-    a{
-        border-radius:2px;
-        border:1px solid;
-        text-decoration:none;
-        padding:6px;
-        color: black;
-    }
-    a:hover{
-        text-decoration:none;
-        color:red;
-        border:1px solid red;
-    }
-"""
-st.markdown(link, unsafe_allow_html=True)
-st.markdown(html, unsafe_allow_html=True)
